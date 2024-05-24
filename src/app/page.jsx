4031ca9c7c42884ec5e5568594bd1d0134b42aad
Ayush bahuguna <ayushbahuguna1122@gmail.com>
2024-05-24 16:04:28 +0530

    kgjrjg

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,12 +5,12 @@ import HeroImage from "@/asset/heroImage.jpg";
 export default function page() {
   return (
     <>
-      <div className="flex relative">
-        <img
+      <div className="flex flex-col">
+        {/* <img
           src={"https://wallpapercave.com/wp/wp10322965.jpg"}
           className="w-dvw h-dvh blur-sm opacity-80"
-        />
-        <nav className="flex item-center justify-between flex-row p-5 absolute top-0 w-dvw">
+        /> */}
+        <nav className="flex item-center justify-between flex-row p-5  w-dvw">
           <p className="font-bold text-xl">Khan-Resturant</p>
           <div className="flex flex-row gap-4">
             <p className="hover:underline cursor-pointer">Home</p>
@@ -19,7 +19,7 @@ export default function page() {
             <p className="hover:underline cursor-pointer">contact us</p>
           </div>
         </nav>
-        <div className="h-dvh w-dvw flex items-start justify-center pt-[100px] -translate-x-[50px] absolute top-0 ">
+        <div className="h-dvh w-dvw flex items-start justify-center ">
           <div className="flex flex-col text-7xl">
             <p className="font-bold underline">Authentic</p>
             <p className=" ps-[70px] text-slate-600">Food </p>
@@ -27,11 +27,45 @@ export default function page() {
           </div>
         </div>
       </div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro dolorum,
-        totam et perferendis omnis eaque earum atque ad odio. Architecto facilis
-        autem reprehenderit iure quos rem commodi dolorum quibusdam voluptatum.
-      </p>
+      <div className="flex bg-slate-50 w-dvw h-dvh flex-col px-28 text-center">
+        <p className="text-center text-5xl font-bold py-5">About</p>
+        <p>
+          At Khan resturant, we believe that dining is more than just eating
+          out; it’s an experience. Nestled in the heart of pantnagar, our
+          restaurant offers a unique blend of [cuisine type, e.g.,
+          Mediterranean, Italian, Asian-fusion] flavors, combining traditional
+          recipes with modern culinary techniques.
+        </p>
+        <p className="text-xl">Our Story</p>
+        <p>
+          Founded in 2024, khan resturant was born out of a passion for
+          exceptional food and heartfelt hospitality. Our founders, Ayush
+          envisioned a place where guests could come together to enjoy delicious
+          meals in a warm, inviting atmosphere. With years of culinary
+          experience and a dedication to sourcing the finest ingredients, they
+          created a menu that celebrates the richness of indian food,
+        </p>
+
+        <p className="text-xl">Our Philosophy</p>
+        <p>
+          We believe in the power of food to bring people together. That’s why
+          we are committed to using only the freshest, locally-sourced
+          ingredients. Our chefs meticulously craft each dish to ensure every
+          bite is a burst of flavor. Whether you’re joining us for a casual
+          lunch, a romantic dinner, or a special celebration, we strive to make
+          every meal memorable.
+        </p>
+
+        <p className="text-xl">Our Menu</p>
+        <p>
+          Our menu features a variety of dishes that cater to all tastes and
+          preferences. From hearty mains and fresh seafood to vegan and
+          gluten-free options, there’s something for everyone at [Restaurant
+          Name]. Don’t miss our signature dishes like [Popular Dish 1] and
+          [Popular Dish 2], as well as our carefully curated wine list and
+          handcrafted cocktails.
+        </p>
+      </div>
     </>
   );
 }
